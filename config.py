d345d5cad7b716e83dc80669c8a391d5cb70b7eb
Ayanokozii <ayanokozi0925@gmail.com>
2025-05-08 21:00:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ class Config:
     API_ID = "21189715"
     API_HASH = "988a9111105fd2f0c5e21c2c2449edfd"
     TOKEN = "8134445150:AAHRDhZp02xmrmUELWrgi9u_ypOCJWi1WMA"
-    MONGO_URL = "mongodb+srv://dostuahi:WeQKx8yemN268htW@cluster0.6fstll3.mongodb.net/?retryWrites=true&w=majority"
+    MONGO_URL = "mongodb+srv://codexkairnex:gm6xSxXfRkusMIug@cluster0.bplk1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     START_PIC = "https://telegra.ph/file/3464fb495af70bebb11fa.jpg"
     SUDOERS = filters.user(["6999372290"])
